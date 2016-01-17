1959e271c67b4566c0cfad45eff90db64f12fc4e
po_qi <poqi1987@gmail.com>
2016-01-17 13:32:30 +0800

    restful interface for linkedme install, open, close, url

---
+++ lkme-web/src/main/java/cc/linkedme/api/lkme/web/link/LMLinkResources.java
@@ -0,0 +1,45 @@
+package cc.linkedme.api.lkme.web.link;
+
+import cc.linkedme.service.LMLinkService;
+import com.google.common.base.Strings;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+@Component
+public class LMLinkResources {
+
+    private LMLinkService lmLinkService;
+
+    public LMLinkService getLmLinkService() {
+        return lmLinkService;
+    }
+
+    public void setLmLinkService(LMLinkService lmLinkService) {
+        this.lmLinkService = lmLinkService;
+    }
+
+    @Path("/l")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String deepLink(@QueryParam("link_click_id") String linkClickId,
+                          @Context HttpServletRequest request) {
+
+        if (Strings.isNullOrEmpty(linkClickId)) {
+
+        }
+
+        String result = lmLinkService.deepLink(linkClickId);
+        return result;
+
+    }
+
+
+
+    
+
+
+}
