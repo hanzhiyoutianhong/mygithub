5762e6ebb18fc53d6a2047738600e8e1a3025ed7
wrshine <i@wrshine.com>
2016-01-20 00:10:49 +0800

    redis&mysql junit test

---
+++ linkedme-commons/src/test/java/cc/linkedme/commons/shard/ShardingSupportHashTest.java
@@ -0,0 +1,48 @@
+package cc.linkedme.commons.shard;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cc.linkedme.commons.redis.JedisPort;
+
+@SuppressWarnings("unchecked")
+public class ShardingSupportHashTest {
+    private static ShardingSupportHash<JedisPort> sh;
+    private static String key;
+    private static long id;
+
+    static {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] {"classpath:spring/redis.xml"});
+        ctx.start();
+        System.out.println("context init sucess!");
+        sh = (ShardingSupportHash<JedisPort>) ctx.getBean("mgetShardingSupport");
+        key = "abcd";
+        id = 12345L;
+    }
+
+    @Test
+    public void testGetClient() {
+        JedisPort client1 = sh.getClient(key);
+        JedisPort client2 = sh.getClient(id);
+
+        client1.set(key, key + "value");
+        client2.set(String.valueOf(id), id + "value");
+
+        System.out.println(client1.get(key));
+        System.out.println(client2.get(key));
+    }
+
+    @Test
+    public void testGetDbTable() {
+        DbTable table1 = sh.getDbTable(key);
+        JedisPort client1 = sh.getClientByDb(table1.getDb());
+        client1.set(key, key + "value");
+        System.out.println(client1.get(key));
+
+        DbTable table2 = sh.getDbTable(id);
+        JedisPort client2 = sh.getClientByDb(table2.getDb());
+        client2.set(String.valueOf(id), id + "value");
+        System.out.println(client2.get(String.valueOf(id)));
+    }
+
+}
