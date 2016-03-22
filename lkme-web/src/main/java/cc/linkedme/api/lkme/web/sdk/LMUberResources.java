ff041e29f39def7033542fa9dad30bbc743d9a27
po_qi <poqi1987@gmail.com>
2016-03-22 13:39:22 +0800

    uber propertities

--- lkme-web/src/main/java/cc/linkedme/api/lkme/web/sdk/LMUberResources.java
+++ lkme-web/src/main/java/cc/linkedme/api/lkme/web/sdk/LMUberResources.java
@@ -1,22 +1,9 @@
 package cc.linkedme.api.lkme.web.sdk;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Component;
-
-import cc.linkedme.commons.exception.LMException;
-import cc.linkedme.commons.exception.LMExceptionFactor;
-import cc.linkedme.commons.json.JsonBuilder;
-import cc.linkedme.commons.util.Constants;
-import cc.linkedme.data.model.params.LMCloseParams;
-import cc.linkedme.data.model.params.LMInstallParams;
-import cc.linkedme.data.model.params.LMOpenParams;
-import cc.linkedme.data.model.params.LMUrlParams;
-import cc.linkedme.service.sdkapi.LMSdkService;
 
 import com.google.common.base.Strings;
 
@@ -24,19 +11,13 @@ import com.google.common.base.Strings;
 @Component
 public class LMUberResources {
 
-    @Resource
-    private LMSdkService lmSdkService;
 
-    public void setLmSdkService(LMSdkService lmSdkService) {
-        this.lmSdkService = lmSdkService;
-    }
 
     @Path("/sdk")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public String sdk(@QueryParam("linkedme_key") String linkedMEKey,
-                          @QueryParam("button_id") String deviceId,
-                          @QueryParam("device_type") Byte deviceType) {
+                          @QueryParam("button_id") String deviceId) {
         if (Strings.isNullOrEmpty(linkedMEKey)) {
 
         }
@@ -54,6 +35,9 @@ public class LMUberResources {
         if (Strings.isNullOrEmpty(linkedMEKey)) {
 
         }
+
+
+
 
         String result = null;
 
