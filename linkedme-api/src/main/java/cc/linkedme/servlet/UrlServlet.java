ab47fc6523ae67ee8bc0b16713feb960269001ac
tianjunfeng.cs@gmail.com <tianjunfeng.cs@gmail.com>
2016-07-04 13:55:15 +0800

    add defered deepling support from chrome

--- linkedme-api/src/main/java/cc/linkedme/servlet/UrlServlet.java
+++ linkedme-api/src/main/java/cc/linkedme/servlet/UrlServlet.java
@@ -1,10 +1,6 @@
 package cc.linkedme.servlet;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +9,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -21,16 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import cc.linkedme.commons.util.Util;
-import cc.linkedme.mcq.ClientMsgPusher;
-import cc.linkedme.mcq.DeepLinkMsgPusher;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import com.google.api.client.repackaged.com.google.common.base.Strings;
-import com.google.common.base.Joiner;
 
 import cc.linkedme.commons.cookie.CookieHelper;
 import cc.linkedme.commons.log.ApiLogger;
@@ -44,12 +32,17 @@ import cc.linkedme.commons.useragent.UserAgent;
 import cc.linkedme.commons.util.Base62;
 import cc.linkedme.commons.util.Constants;
 import cc.linkedme.commons.util.MD5Utils;
+import cc.linkedme.commons.util.Util;
 import cc.linkedme.commons.util.UuidHelper;
 import cc.linkedme.commons.uuid.UuidCreator;
 import cc.linkedme.data.model.AppInfo;
 import cc.linkedme.data.model.DeepLink;
+import cc.linkedme.mcq.DeepLinkMsgPusher;
 import cc.linkedme.service.DeepLinkService;
 import cc.linkedme.service.webapi.AppService;
+
+import com.google.api.client.repackaged.com.google.common.base.Strings;
+import com.google.common.base.Joiner;
 
 /**
  * Created by LinkedME01 on 16/4/1.
@@ -425,16 +418,23 @@ public class UrlServlet extends HttpServlet {
 
         request.setAttribute("DEBUG", DEBUG);
 
-        // if ((!isWechat) && (!isWeibo) && isAndroid && isChrome && userAgentMajor >= 25 &&
-        // !isMIUI) {
-        // String location = "intent://linkedme?click_id=" + uriArr[2] + "#Intent;scheme=" + scheme
-        // + ";package="
-        // + appInfo.getAndroid_package_name() + ";S.browser_fallback_url=" + url + ";end";
-        // response.setStatus(307);
-        // response.setHeader("Location", location);
-        // // response.sendRedirect(location);
-        // return;
-        // }
+        if ((!isWechat) && (!isWeibo) && isAndroid && isChrome && userAgentMajor >= 25 && !isMIUI) {
+            String browser_fallback_url = new StringBuilder(Constants.DEEPLINK_HTTP_PREFIX).append("i/js/record_id_and_redirect?")
+                    .append("identity_id=").append(identityId).append("&")
+                    .append("app_id=").append(appId).append("&")
+                    .append("is_valid_identityid=").append(isValidIdentity).append("&")
+                    .append("browser_fingerprint_id=").append(browserFingerprintId).append("&")
+                    .append("deeplink_id=").append(deepLinkId).append("&")
+                    .append("url=").append(url).toString();
+                    
+            String location =
+                    "intent://linkedme?click_id=" + uriArr[2] + "#Intent;scheme=" + scheme + ";package="
+                            + appInfo.getAndroid_package_name() + ";S.browser_fallback_url=" + browser_fallback_url + ";end";
+            response.setStatus(307);
+            response.setHeader("Location", location);
+            response.sendRedirect(location);
+            return;
+        }
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/linkedme.jsp");
         dispatcher.forward(request, response);
