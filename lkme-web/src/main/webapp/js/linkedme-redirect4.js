991655700b8fbbb35ebf1fd6130c3d43b03d9249
wrshine <i@wrshine.com>
2016-07-06 14:22:06 +0800

    record js event

--- lkme-web/src/main/webapp/js/linkedme-redirect4.js
+++ lkme-web/src/main/webapp/js/linkedme-redirect4.js
@@ -1,6 +1,6 @@
 var lkmeAction = {
-    recordIdUrl: "/i/js/record_id",
-    recordJsEventUrl: "/i/js/record_event",
+    recordIdUrl: "/" + Params.live_test_flag + "/js/record_id",
+    recordJsEventUrl: "/" + Params.live_test_flag + "/js/record_event",
     recordJsUserClickEventUrl: "/i/js/record_click_event",
     destination: {
         iOSScheme: "dst_ios_scheme",
