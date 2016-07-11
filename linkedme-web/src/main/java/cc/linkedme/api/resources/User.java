2415473800d2dc71297ec38ff03b769689cf3e6c
dunso <admin@dun.so>
2016-07-11 20:28:21 +0800

    modify the validation of user moudle

--- linkedme-web/src/main/java/cc/linkedme/api/resources/User.java
+++ linkedme-web/src/main/java/cc/linkedme/api/resources/User.java
@@ -11,6 +11,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Component;
 
 import cc.linkedme.commons.exception.LMException;
@@ -37,11 +39,20 @@ public class User {
     @POST
     @Produces({MediaType.APPLICATION_JSON})
     public String register(UserParams userParams, @Context HttpServletRequest request) {
-        if (userParams.email != null)
+        JSONArray jsonArray = new JSONArray();
+
+        if (Strings.isNullOrEmpty(userParams.email)) {
+            jsonArray.add(emailIsNull());
+        }else if(userService.validateEmail(userParams)){
+            jsonArray.add(emailExist());
+        }else{
             userParams.email = userParams.email.toLowerCase();
-        else {
-            throw new LMException(LMExceptionFactor.LM_ILLEGAL_PARAM_VALUE, "Email should not be empty");
         }
+
+        if(jsonArray.size() > 0){
+            return jsonArray.toString();
+        }
+
         if (userService.userRegister(userParams)) {
             JsonBuilder resultJson = new JsonBuilder();
             resultJson.append("ret", "true");
@@ -56,11 +67,28 @@ public class User {
     @Produces({MediaType.APPLICATION_JSON})
 
     public String login(UserParams userParams, @Context HttpServletRequest request) {
-        if (userParams.email != null)
+        JSONArray jsonArray = new JSONArray();
+        boolean emailIsValid = false;
+
+        if (Strings.isNullOrEmpty(userParams.email)) {
+            jsonArray.add(emailIsNull());
+        }else if(!userService.validateEmail(userParams)){
+            jsonArray.add(emailNotExist());
+        }else{
+            emailIsValid = true;
             userParams.email = userParams.email.toLowerCase();
-        else {
-            throw new LMException(LMExceptionFactor.LM_ILLEGAL_PARAM_VALUE, "Email should not be empty");
         }
+
+        if(Strings.isNullOrEmpty(userParams.pwd)){
+            jsonArray.add(passwordIsNull());
+        }else if(emailIsValid && !userService.validatePassword(userParams.email,userParams.pwd)){
+            jsonArray.add(passwordErr());
+        }
+
+        if(jsonArray.size() > 0){
+            return jsonArray.toString();
+        }
+
         String email = userParams.email;
         UserInfo userInfo = userService.userLogin(userParams);
         JsonBuilder resultJson = new JsonBuilder();
@@ -120,10 +148,30 @@ public class User {
     @POST
     @Produces({MediaType.APPLICATION_JSON})
     public String change_password(UserParams userParams, @Context HttpServletRequest request) {
-        if (userParams.email != null)
+        JSONArray jsonArray = new JSONArray();
+        boolean emailIsValid = false;
+
+        if (Strings.isNullOrEmpty(userParams.email)) {
+            jsonArray.add(emailIsNull());
+        }else if(!userService.validateEmail(userParams)){
+            jsonArray.add(emailNotExist());
+        }else{
+            emailIsValid = true;
             userParams.email = userParams.email.toLowerCase();
-        else {
-            throw new LMException(LMExceptionFactor.LM_ILLEGAL_PARAM_VALUE, "Email should not be empty");
+        }
+
+        if(Strings.isNullOrEmpty(userParams.old_pwd)){
+            jsonArray.add(oldPasswordIsNull());
+        }else if(emailIsValid && !userService.validatePassword(userParams.email,userParams.old_pwd)){
+            jsonArray.add(oldPasswordErr());
+        }
+
+        if(Strings.isNullOrEmpty(userParams.new_pwd)){
+            jsonArray.add(newPasswordIsNull());
+        }
+
+        if(jsonArray.size() > 0){
+            return jsonArray.toString();
         }
 
         if (userService.resetUserPwd(userParams)) {
@@ -139,10 +187,17 @@ public class User {
     @POST
     @Produces({MediaType.APPLICATION_JSON})
     public String forgot_password(UserParams userParams, @Context HttpServletRequest request) {
-        if (userParams.email != null)
+
+        JSONArray jsonArray = new JSONArray();
+        if (userParams.email == null) {
+            jsonArray.add(emailIsNull());
+        }else if(!userService.validateEmail(userParams)){
+            jsonArray.add(emailNotExist());
+        }else{
             userParams.email = userParams.email.toLowerCase();
-        else {
-            throw new LMException(LMExceptionFactor.LM_ILLEGAL_PARAM_VALUE, "Email should not be empty");
+        }
+        if(jsonArray.size() > 0){
+            return jsonArray.toString();
         }
 
         if (userService.forgotPwd(userParams)) {
@@ -158,10 +213,13 @@ public class User {
     @POST
     @Produces({MediaType.APPLICATION_JSON})
     public String set_password(UserParams userParams, @Context HttpServletRequest request) {
+        JSONArray jsonArray = new JSONArray();
         if (Strings.isNullOrEmpty(userParams.new_pwd)) {
-            throw new LMException(LMExceptionFactor.LM_ILLEGAL_PARAM_VALUE, "Email should not be empty");
+            jsonArray.add(passwordIsNull());
         }
-
+        if(jsonArray.size() > 0){
+            return jsonArray.toString();
+        }
         if (userService.resetForgottenPwd(userParams)) {
             JsonBuilder resultJson = new JsonBuilder();
             resultJson.append("ret", "true");
@@ -179,6 +237,70 @@ public class User {
         JsonBuilder resultJson = new JsonBuilder();
         resultJson.append("ret", result);
         return resultJson.flip().toString();
+    }
+
+    public JSONObject emailIsNull(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40000");
+        jsonObject.put("err_param","email");
+        jsonObject.put("err_msg","未填写邮箱!");
+        return jsonObject;
+    }
+
+    public JSONObject emailNotExist(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40005");
+        jsonObject.put("err_param","email");
+        jsonObject.put("err_msg","邮箱不存在!");
+        return jsonObject;
+    }
+
+    public JSONObject emailExist(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40004");
+        jsonObject.put("err_param","email");
+        jsonObject.put("err_msg","邮箱已被注册");
+        return jsonObject;
+    }
+
+    public JSONObject passwordErr(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40006");
+        jsonObject.put("err_param","pwd");
+        jsonObject.put("err_msg","密码错误!");
+        return jsonObject;
+    }
+
+    public JSONObject passwordIsNull(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40006");
+        jsonObject.put("err_param","pwd");
+        jsonObject.put("err_msg","密码为空!");
+        return jsonObject;
+    }
+
+    public JSONObject oldPasswordIsNull(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40006");
+        jsonObject.put("err_param","pwd");
+        jsonObject.put("err_msg","旧密码为空!");
+        return jsonObject;
+    }
+
+    public JSONObject oldPasswordErr(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40006");
+        jsonObject.put("err_param","pwd");
+        jsonObject.put("err_msg","旧密码错误!");
+        return jsonObject;
+    }
+
+    public JSONObject newPasswordIsNull(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("err_code","40006");
+        jsonObject.put("err_param","pwd");
+        jsonObject.put("err_msg","新密码为空!");
+        return jsonObject;
     }
 
 }
