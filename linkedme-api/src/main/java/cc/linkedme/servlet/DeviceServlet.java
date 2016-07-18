6d31094ac05905200730ee86834662748e96aae8
dunso <admin@dun.so>
2016-07-18 20:25:36 +0800

    fix bugs

--- linkedme-api/src/main/java/cc/linkedme/servlet/DeviceServlet.java
+++
@@ -1,7 +0,0 @@
-package cc.linkedme.servlet;
-
-/**
- * Created by LinkedME07 on 16/7/18.
- */
-public class DeviceServlet {
-}
--- linkedme-api/src/main/java/cc/linkedme/thread/DeviceThread.java
+++
@@ -1,8 +0,0 @@
-package cc.linkedme.thread;
-
-/**
- * Created by LinkedME07 on 16/7/18.
- */
-public interface DeviceThread {
-    public void start();
-}
